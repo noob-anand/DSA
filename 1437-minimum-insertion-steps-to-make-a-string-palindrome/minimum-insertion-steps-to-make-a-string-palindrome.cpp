9b65d5d1902517b722c2b187e4a13f36f0f3d04d
Anand Sharma <anandsharma8015@gmail.com>
2026-06-24 11:02:19 +0530

    Time: 19 ms (88.82%) | Memory: 9.8 MB (86.02%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int minInsertions(string s1) {
-        string s2 = s1;
-        reverse(s1.begin(), s1.end());
-        int n = s1.size(),m=n;
-
-        vector<int> prev(m + 1, 0), cur(m + 1, 0);
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s1[i - 1] == s2[j - 1])
-                    cur[j] = 1 + prev[j - 1];
-                else
-                    cur[j] = max(prev[j], cur[j - 1]);
-            }
-            prev = cur;
+    int lcs(string& s, string& t){
+        int n= s.size();
+        int m= t.length();
+        vector<int> prev(m+1,0), curr(m+1,0);
+        for(int j=0;j<=m;j++){
+            prev[j]=0;
         }
-
-        return (n-prev[m]);
+        for(int i=0;i<=n;i++){
+            curr[0]=0;
+        }
+        for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+        if(s[i-1]==t[j-1]){
+            curr[j] = 1+prev[j-1];
+        }
+        else{
+            curr[j] = max(curr[j-1],prev[j]);
+        }
+        }
+        prev = curr;
+        }
+        return prev[m];
+    }
+    int longestpalindrome(string s){
+        string t= s;
+        reverse(t.begin(),t.end());
+        return lcs(s,t);
+    }
+    int minInsertions(string s) {
+        int n= s.size();
+        return n-longestpalindrome(s);
     }
 };
