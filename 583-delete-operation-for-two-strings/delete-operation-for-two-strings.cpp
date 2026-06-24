5d1a261cf962895a9e083290d8ccd62e6a82aae1
Anand Sharma <anandsharma8015@gmail.com>
2026-06-24 11:09:47 +0530

    Time: 18 ms (16.83%) | Memory: 16.7 MB (70.37%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int minDistance(string s1, string s2) {
-        int n = s1.size(), m = s2.size();
+    int f(int i, int j, string& word1, string& word2, vector<vector<int>>& dp) {
+        if (i < 0 || j < 0)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        vector<int> prev(m + 1, 0), cur(m + 1, 0);
+        if (word1[i] == word2[j])
+            return dp[i][j] = 1 + f(i - 1, j - 1, word1, word2, dp);
+        else
+            return dp[i][j] = max(f(i, j - 1, word1, word2, dp),
+                                  f(i - 1, j, word1, word2, dp));
+    }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s1[i - 1] == s2[j - 1])
-                    cur[j] = 1 + prev[j - 1];
-                else
-                    cur[j] = max(prev[j], cur[j - 1]);
-            }
-            prev = cur;
-        }
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
 
-        return ((n+m)-(prev[m]*2));
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        int count = f(n - 1, m - 1, word1, word2, dp);
+
+        return n - count + m - count;
     }
 };
