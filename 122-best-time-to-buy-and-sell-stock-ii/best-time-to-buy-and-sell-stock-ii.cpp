1e14bb86446a743f6aca46847d96403e25a6fe8a
Anand Sharma <anandsharma8015@gmail.com>
2026-06-26 08:50:38 +0530

    Time: 8 ms (33.86%) | Memory: 24.9 MB (19.10%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -17,9 +17,25 @@ public:
         }
         return dp[i][b] = p;
     }
+
     int maxProfit(vector<int>& a) {
         int n = a.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return f(0, 1, a, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int b = 0; b < 2; b++) {
+                int p;
+                if (b) {
+                    int nt = dp[i + 1][1];
+                    int t = dp[i + 1][0] - a[i];
+                    p = max(t, nt);
+                } else {
+                    int s = dp[i + 1][1] + a[i];
+                    int ns = dp[i + 1][0];
+                    p = max(s, ns);
+                }
+                dp[i][b] = p;
+            }
+        }
+        return dp[0][1];
     }
 };
