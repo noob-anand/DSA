981d01f304289910b8ef7d8beafff4ebc305867e
Anand Sharma <anandsharma8015@gmail.com>
2026-06-27 13:28:12 +0530

    Time: 45 ms (70.16%) | Memory: 79.4 MB (71.45%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int f(int i, int b, int n, vector<int>& a,
-          vector<vector<vector<int>>>& dp) {
-        if (n == 0)
-            return 0;
-        if (i == a.size())
-            return 0;
-        if (dp[i][b][n] != -1)
-            return dp[i][b][n];
-        int p;
-        if (b) {
-            int nt = f(i + 1, 1, n, a,dp);
-            int t = f(i + 1, 0, n, a,dp) - a[i];
-            p = max(t, nt);
-        } else {
-            int s = f(i + 1, 1, n - 1, a,dp) + a[i];
-            int ns = f(i + 1, 0, n, a,dp);
-            p = max(s, ns);
-        }
-        return dp[i][b][n] = p;
-    }
     int maxProfit(vector<int>& a) {
-        vector<vector<vector<int>>> dp(
-            a.size(), vector<vector<int>>(2, vector<int>(3, -1)));
-        return f(0, 1, 2, a, dp);
+        vector<vector<int>> prev(2, vector<int>(3, 0));
+        vector<vector<int>> cur(2, vector<int>(3, 0));
+        for (int i = a.size() - 1; i >= 0; i--) {
+            for (int b = 0; b <= 1; b++) {
+                for (int n = 1; n <= 2; n++) {
+                    if (b) {
+                        int buy = prev[0][n] - a[i];
+                        int skip = prev[1][n];
+                        cur[b][n] = max(buy, skip);
+                    } else {
+                        int sell = prev[1][n - 1] + a[i];
+                        int skip = prev[0][n];
+                        cur[b][n] = max(sell, skip);
+                    }
+                }
+            }
+            prev = cur;
+        }
+        return prev[1][2];
     }
 };
