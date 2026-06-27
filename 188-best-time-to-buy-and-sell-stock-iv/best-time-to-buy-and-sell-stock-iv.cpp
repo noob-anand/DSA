7ef92d1e0b14ebb9729b179bb66f8256ae39a2fa
Anand Sharma <anandsharma8015@gmail.com>
2026-06-27 13:45:34 +0530

    Time: 4 ms (71.61%) | Memory: 14.3 MB (87.96%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& a) {
-        int m = a.size();
-        vector<vector<vector<int>>> dp(
-            m + 1, vector<vector<int>>(2, vector<int>(k+1, 0)));
-        for (int i = m - 1; i >= 0; i--) {
+        vector<vector<int>> prev(2, vector<int>(k + 1, 0));
+        vector<vector<int>> cur(2, vector<int>(k + 1, 0));
+        for (int i = a.size() - 1; i >= 0; i--) {
             for (int b = 0; b <= 1; b++) {
                 for (int n = 1; n <= k; n++) {
                     if (b) {
-                        int buy = dp[i + 1][0][n] - a[i];
-                        int skip = dp[i + 1][1][n];
-                        dp[i][b][n] = max(buy, skip);
+                        int buy = prev[0][n] - a[i];
+                        int skip = prev[1][n];
+                        cur[b][n] = max(buy, skip);
                     } else {
-                        int sell = dp[i + 1][1][n - 1] + a[i];
-                        int skip = dp[i + 1][0][n];
-                        dp[i][b][n] = max(sell, skip);
+                        int sell = prev[1][n - 1] + a[i];
+                        int skip = prev[0][n];
+                        cur[b][n] = max(sell, skip);
                     }
                 }
             }
+            prev = cur;
         }
-
-        return dp[0][1][k];
+        return prev[1][k];
     }
 };
