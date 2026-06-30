e1026a1735092df963c2eb85af65af844741e2f2
Anand Sharma <anandsharma8015@gmail.com>
2026-06-30 15:34:55 +0530

    Time: 4 ms (79.36%) | Memory: 11 MB (57.43%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int a=0,b=0,c=0;
-        int i=0;
-        int ans =0;
+        int a = 0, b = 0, c = 0;
+        int i = 0;
+        int ans = 0;
         int pre = 0;
-        for(int j=0;j<s.size();j++){
-            if(s[j]=='a') a++;
-            if(s[j]=='b') b++;
-            if(s[j]=='c') c++;
-            while(a>0&&b>0&&c>0){
-                 if(s[i]=='a') a--;
-            if(s[i]=='b') b--;
-            if(s[i]=='c') c--;
-            i++;
-            
+        for (int j = 0; j < s.size(); j++) {
+            if (s[j] == 'a')
+                a++;
+            if (s[j] == 'b')
+                b++;
+            if (s[j] == 'c')
+                c++;
+            while (a && b && c) {
+                if (s[i] == 'a')
+                    a--;
+                if (s[i] == 'b')
+                    b--;
+                if (s[i] == 'c')
+                    c--;
+                i++;
             }
-            ans+=i;
+            ans += i;
         }
         return ans;
     }
